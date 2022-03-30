47f78bb2766c6f69dbaaab75782a07c7cad2eac9
Allister William Gray Gardner <allister.w.g.gardner@gmail.com>
2022-03-30 12:31:30 +0100

    git commit -m"please give me my files back"

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,11 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `item_description` VARCHAR(200) DEFAULT NULL,
+    `item_price` FLOAT DEFAULT NULL,
+    PRIMARY KEY (item_id)
+);
